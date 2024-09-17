864d2b696133a2811a3dd6dc2490f90537c5b040
lleonardus <leonardus.wizard@gmail.com>
2024-09-17 19:24:47 -0300

    adding colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      black: "#000",
+      white: "#fff",
+      beaver: "#9e7f66",
+      "cod-gray": "#111111",
+      mirage: "#17192b",
+      "ebony-clay": "#242b37",
+      "shuttle-gray": "#5c6779",
+    },
   },
   plugins: [],
-}
+};
