ad3d7f7ffe1217e73b399837685a99f93a90fadd
lleonardus <leonardus.wizard@gmail.com>
2024-09-20 11:03:45 -0300

    removing grid rows from layout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { Footer } from "./Footer";
+//TODO(done) removing grid rows 560
 
 export function AppLayout() {
   return (
     <div className="flex h-screen w-screen justify-center font-spartan">
-      <div className="grid h-full w-full max-w-[1920px] grid-rows-3">
+      <div className="grid h-full w-full max-w-[1920px]">
         <Outlet />
         <Footer />
       </div>
