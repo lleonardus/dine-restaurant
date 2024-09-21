3b475451383c3f171aedd2f64a685585d59f7442
lleonardus <leonardus.wizard@gmail.com>
2024-09-20 23:24:22 -0300

    fixing bug in button animation

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,19 +1,22 @@
 import { Link } from "react-router-dom";
 
+//TODO fixing bug in button animation
+//last commit: 1721
+
 export function Button({ children, theme, to, disabled }) {
-  const base = `relative z-[9999] inline-block py-[18px] text-center 
+  const base = `relative z-[9999] overflow-hidden inline-block py-[18px] text-center 
                 text-heading-sm uppercase outline outline-1 transition-[color]
                 delay-75 ease-in after:absolute after:left-[50%] after:top-0
-                after:z-[-1] after:h-full after:w-0 after:translate-x-[-50%]
+                after:z-[-1] after:h-[110%] after:hover:outline after:w-0 after:translate-x-[-50%]
                 after:bg-white after:transition-[width] after:duration-[220ms]
-                after:ease-in after:hover:w-full hover:text-cod-gray 
+                after:ease-in after:hover:w-[110%] hover:text-cod-gray 
                 disabled:cursor-not-allowed disabled:bg-[transparent]
                 disabled:text-cod-gray/30 disabled:outline-cod-gray/30 
                 disabled:hover:bg-[transparent]`;
 
   const styles = {
-    dark: `${base} bg-[transparent] text-white outline-white hover:outline-white`,
-    light: `${base} bg-cod-gray text-white outline-cod-gray hover:outline-cod-gray`,
+    dark: `${base} bg-[transparent] text-white outline-white hover:outline-white after:hover:outline-white`,
+    light: `${base} bg-cod-gray text-white outline-cod-gray hover:outline-cod-gray after:hover:outline-cod-gray`,
   };
 
   if (to) {
