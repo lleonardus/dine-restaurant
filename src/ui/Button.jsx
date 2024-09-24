28d764e698a5129f17e52a0f19b734860190fab1
lleonardus <leonardus.wizard@gmail.com>
2024-09-24 05:29:50 -0300

    adding additionalClasses property

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
 
-export function Button({ children, theme, to, disabled, onClick }) {
+export function Button({
+  children,
+  additionalClasses,
+  theme,
+  to,
+  disabled,
+  onClick,
+}) {
   const base = `relative z-[9999] overflow-hidden inline-block py-[18px] text-center 
                 text-heading-sm uppercase outline outline-1 transition-[color]
                 delay-75 ease-in after:absolute after:left-[50%] after:top-0
@@ -9,7 +16,7 @@ export function Button({ children, theme, to, disabled, onClick }) {
                 after:ease-in after:hover:w-[110%] hover:text-cod-gray 
                 disabled:cursor-not-allowed disabled:bg-[transparent]
                 disabled:text-cod-gray/30 disabled:outline-cod-gray/30 
-                disabled:hover:bg-[transparent]`;
+                disabled:hover:bg-[transparent] ${additionalClasses}`;
 
   const styles = {
     dark: `${base} bg-[transparent] text-white outline-white hover:outline-white after:hover:outline-white`,
@@ -31,7 +38,7 @@ export function Button({ children, theme, to, disabled, onClick }) {
   return (
     <button
       onClick={() => onClick?.()}
-      className={`${styles[theme]} w-full`}
+      className={styles[theme]}
       type="submit"
       disabled={disabled}
     >
