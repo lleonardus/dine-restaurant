f8ef99556d0928dfbf5a8641f43bf6bbb60595cd
lleonardus <leonardus.wizard@gmail.com>
2024-09-24 18:18:16 -0300

    replacing div with background image with picture tag to responsively display the photo of the dish and improve code readability

--- src/pages/homepage/components/Header.jsx
+++ src/pages/homepage/components/Header.jsx
@@ -2,13 +2,29 @@ import { Button } from "../../../ui/Button";
 
 export function Header() {
   return (
-    <header>
-      <div className="bg-cod-gray bg-[url('/images/homepage/hero-bg-mobile.jpg')] bg-auto bg-top bg-no-repeat pt-[220px] tablet:bg-[url('/images/homepage/hero-bg-tablet.jpg')] tablet:pt-[373px] xl:hidden"></div>
-      <div className="relative flex flex-col items-center bg-black px-6 pb-[152px] pt-3 text-white tablet:pb-[216px] xl:z-[9999] xl:items-start xl:bg-[url('/images/homepage/hero-bg-desktop.jpg')] xl:bg-contain xl:bg-right xl:bg-no-repeat xl:px-[165px] xl:pb-[198px] xl:before:absolute xl:before:left-0 xl:before:top-0 xl:before:z-[-1] xl:before:h-full xl:before:w-[545px] xl:before:bg-black xl:after:absolute xl:after:left-[545px] xl:after:top-0 xl:after:z-[-1] xl:after:h-full xl:after:w-[235px] xl:after:bg-cod-gray desktop:px-[240px] desktop:before:w-[845px] desktop:after:left-[620px] desktop:after:w-[230px]">
-        <div className="xl:pt-[65px]">
+    <header className="relative xl:flex xl:flex-row-reverse">
+      <div className="relative bg-cod-gray pt-[220px] tablet:pt-[369px] xl:z-[9999] xl:w-[60%]">
+        <picture className="absolute right-[50%] top-0 w-[375px] translate-x-[50%] tablet:w-[768px] xl:right-0 xl:w-[1370px] xl:translate-x-0 desktop:w-[1551px]">
+          <source
+            media="(min-width: 1280px)"
+            srcSet="/images/homepage/hero-bg-desktop.jpg"
+          />
+          <source
+            media="(min-width: 768px)"
+            srcSet="/images/homepage/hero-bg-tablet.jpg"
+          />
+          <img
+            className="xl:h-[855px] xl:w-full"
+            src="/images/homepage/hero-bg-mobile.jpg"
+            alt="shrimp pasta"
+          />
+        </picture>
+      </div>
+      <div className="relative flex flex-col items-center bg-black px-6 pb-[152px] pt-3 text-white tablet:pb-[216px] xl:z-[9999] xl:shrink-0 xl:items-start xl:bg-cod-gray xl:pb-[198px] xl:pl-[165px] xl:pr-0 xl:before:absolute xl:before:left-0 xl:before:top-0 xl:before:-z-10 xl:before:h-full xl:before:w-[544px] xl:before:bg-black desktop:pl-[240px] desktop:before:w-[620px]">
+        <div className="h-8 w-[82.4px] *:h-full *:w-full tablet:h-10 tablet:w-[103px] xl:mt-[65px]">
           <img src="/images/logo.svg" alt="logo" />
         </div>
-        <div className="space-y-[21px] pb-[53px] pt-9 text-center tablet:space-y-5 tablet:pb-[52px] xl:text-left">
+        <div className="space-y-[21px] pb-[53px] pt-9 text-center tablet:space-y-5 tablet:pb-[52px] xl:pt-[153px] xl:text-left">
           <h1 className="mx-auto max-w-[250px] text-[32px] font-light leading-10 tracking-[-0.4px] tablet:max-w-[300px] tablet:text-[48px] tablet:leading-[64px] tablet:tracking-[-0.6px] xl:m-0 xl:max-w-[580px] xl:text-heading-xl">
             Exquisite dining since 1989
           </h1>
