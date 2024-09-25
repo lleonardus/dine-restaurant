dd788ce3e21a690ca0b71f11eaacf1ded9ebf61b
lleonardus <leonardus.wizard@gmail.com>
2024-09-25 00:28:02 -0300

    creating the Frame component as a container for the Gallery component

--- src/pages/homepage/components/events-and-gatherings/EventsAndGatheringsSection.jsx
+++ src/pages/homepage/components/events-and-gatherings/EventsAndGatheringsSection.jsx
@@ -5,6 +5,7 @@ import { ImageItem } from "./ImageItem";
 import { SelectorList } from "./SelectorList";
 import { SelectorItem } from "./SelectorItem";
 import { Button } from "../../../../ui/Button";
+import { Frame } from "./Frame";
 
 const data = [
   {
@@ -26,7 +27,7 @@ export function EventsAndGatheringsSection() {
 
   return (
     <section className="bg-white pb-[124px] pt-20 text-center text-ebony-clay tablet:bg-[url('/images/patterns/pattern-curve-top-right.svg')] tablet:bg-[length:50%_360px] tablet:bg-no-repeat tablet:py-[120px] xl:flex xl:gap-[125px] xl:px-[165px] xl:py-40 desktop:bg-[length:643px_360px] desktop:px-[240px] desktop:pr-0">
-      <div className="mx-auto mb-[49px] h-[400px] w-[326px] shrink-0 bg-shuttle-gray/20 shadow-[0px_100px_80px_-30px_rgba(0,0,0,0.25)] tablet:relative tablet:mb-[57px] tablet:h-[360px] tablet:w-[573px] tablet:overflow-visible tablet:shadow-[0px_100px_80px_-40px_rgba(0,0,0,0.25)] tablet:after:absolute tablet:after:-left-[58px] tablet:after:-top-[38px] tablet:after:h-[76px] tablet:after:w-40 tablet:after:bg-[url('/images/patterns/pattern-lines.svg')] xl:relative xl:m-0 xl:mb-0 xl:h-[600px] xl:w-[540px] xl:bg-center desktop:mb-0">
+      <Frame>
         <Gallery>
           <ImageList current={current}>
             {data.map((item, id) => {
@@ -34,7 +35,7 @@ export function EventsAndGatheringsSection() {
             })}
           </ImageList>
         </Gallery>
-      </div>
+      </Frame>
       <div className="xl:mb-[60px] xl:flex xl:flex-col-reverse">
         <SelectorList>
           {data.map((item, id) => (
