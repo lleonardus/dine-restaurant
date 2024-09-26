a38dd1781ead82759f98d65473310be755de7d37
lleonardus <leonardus.wizard@gmail.com>
2024-09-26 00:59:25 -0300

    rewriting ReservationForm using react hooks form

--- src/pages/booking/components/ReservationForm.jsx
+++ src/pages/booking/components/ReservationForm.jsx
@@ -1,92 +1,129 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { Button } from "../../../ui/Button";
 
 export function ReservationForm() {
-  const [name, setName] = useState("");
-  const [nameError, setNameError] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [email, setEmail] = useState("");
-  const [month, setMonth] = useState("");
-  const [day, setDay] = useState("");
-  const [year, setYear] = useState("");
-  const [dateError, setDateError] = useState("");
-  const [hour, setHour] = useState("");
-  const [minutes, setMinutes] = useState("");
   const [period, setPeriod] = useState("AM");
   const [isOpen, setIsOpen] = useState(false);
-  const [timeError, setTimeError] = useState("");
   const [numberOfPeople, setNumberOfPeople] = useState(4);
 
-  function handleInputChange(e, setInputValue, setInputError) {
-    setInputValue(e.target.value);
-    setInputError(false);
+  const currentDate = new Date();
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    clearErrors,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    mode: "onChange",
+    defaultValues: {
+      year: currentDate.getFullYear(),
+    },
+  });
+
+  const dateError = errors.month || errors.day || errors.year;
+  const timeError = errors.hour || errors.minutes;
+
+  function clearListOfErrors(list) {
+    list.forEach((error) => clearErrors(error));
   }
 
-  function isEmpty(string) {
-    return string.trim() === "";
+  function isDateAvailable(date) {
+    const now = new Date();
+
+    return now.getTime() <= date.getTime();
   }
 
-  function isEmail(email) {
-    const regex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+  function isTimeWithinRange(
+    startHour,
+    startMinutes,
+    endHour,
+    endMinutes,
+    date,
+  ) {
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
 
-    return regex.test(email);
+    return (
+      (hours > startHour || (hours === startHour && minutes >= startMinutes)) &&
+      (hours < endHour || (hours === endHour && minutes <= endMinutes))
+    );
   }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (isEmpty(name)) {
-      setNameError(true);
-      return;
-    }
+  function isTimeAvailable(date) {
+    const isWeekend = date.getDay() === 0 || date.getDay() === 6;
 
-    if (isEmpty(email)) {
-      setEmailError("This field is required");
-      return;
-    }
+    return (
+      (isWeekend && isTimeWithinRange(9, 0, 23, 30, date)) ||
+      (!isWeekend && isTimeWithinRange(9, 0, 22, 0, date))
+    );
+  }
 
-    if (!isEmail(email)) {
-      setEmailError("Please use a valid email address");
-      return;
-    }
+  async function onSubmit(formData) {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    if (isEmpty(month) || isEmpty(day) || isEmpty(year)) {
-      setDateError("This field is incomplete");
-      return;
-    }
+    const { name, email, day, month, year, hour, minutes } = formData;
+    const date = new Date(
+      year,
+      month - 1,
+      day,
+      period === "PM" && hour < 12 ? Number.parseInt(hour) + 12 : hour,
+      minutes,
+    );
 
-    if (isEmpty(hour) || isEmpty(minutes) || isEmpty(period)) {
-      setTimeError("This field is incomplete");
-      return;
+    if (!isDateAvailable(date)) {
+      setError("month", { type: "custom", message: "Unavailable date" });
+    } else if (!isTimeAvailable(date)) {
+      setError("hour", { type: "custom", message: "Unavailable time" });
+    } else {
+      const formattedDate = date.toLocaleDateString("en-US");
+      const formattedTime = date.toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+      });
+      const formatted = `${formattedDate} at ${formattedTime.toLowerCase()}`;
+      alert(
+        `Hello, ${name}. Your reservation for ${numberOfPeople} ${numberOfPeople > 1 ? "people" : "person"} on ${formatted} has been made successfully! For more information, see the email that we sent to ${email}`,
+      );
     }
-
-    console.log("submited");
   }
 
   return (
     <form
       id="form"
-      onSubmit={handleSubmit}
+      onSubmit={handleSubmit(onSubmit)}
       className="absolute right-[50%] flex min-w-[327px] translate-x-[50%] translate-y-[-137px] flex-col bg-white px-8 pb-8 pt-[34px] shadow-[0px_100px_80px_-30px_rgba(0,0,0,0.25)] tablet:w-[540px] tablet:translate-y-[-230px] tablet:px-12 tablet:py-12 xl:right-[50%] xl:translate-x-[100%] xl:translate-y-[-341px] xl:before:absolute xl:before:bottom-0 xl:before:left-0 xl:before:h-[76px] xl:before:w-40 xl:before:-translate-x-20 xl:before:translate-y-[23px] xl:before:bg-[url('/images/patterns/pattern-lines.svg')] xl:before:bg-cover xl:before:bg-no-repeat xl:after:absolute xl:after:bottom-0 xl:after:left-0 xl:after:h-20 xl:after:w-20 xl:after:bg-white"
     >
       <div className="mb-[34px]">
         <input
-          className={`${nameError ? "border-red placeholder:text-red/50" : ""}`}
           placeholder="Name"
           type="text"
-          value={name}
-          onChange={(e) => handleInputChange(e, setName, setNameError)}
+          className={`${errors.name ? "border-red placeholder:text-red/50" : ""}`}
+          {...register("name", {
+            required: "This field is required",
+            onChange: () => clearErrors("name"),
+          })}
         />
-        {nameError && <ErrorMessage>This field is required</ErrorMessage>}
+        {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
       </div>
       <div className="mb-[34px]">
         <input
-          className={`${emailError ? "border-red placeholder:text-red/50" : ""}`}
-          value={email}
-          onChange={(e) => handleInputChange(e, setEmail, setEmailError)}
           placeholder="Email"
           type="text"
+          className={`${errors.email ? "border-red placeholder:text-red/50" : ""}`}
+          onChange={() => clearErrors("name")}
+          {...register("email", {
+            required: "This field is required",
+            pattern: {
+              value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+              message: "Please use a valid email address",
+            },
+            onChange: () => clearErrors("name"),
+          })}
         />
-        {emailError && <ErrorMessage>{emailError}</ErrorMessage>}
+        {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
       </div>
       <div className="mb-[34px] flex flex-col gap-2 tablet:flex-row tablet:items-center tablet:gap-[65px]">
         <div className="relative shrink-0">
@@ -98,32 +135,52 @@ export function ReservationForm() {
           </label>
           {dateError && (
             <ErrorMessage className="max-tablet:absolute max-tablet:left-[100px] max-tablet:top-0">
-              {dateError}
+              {errors.month?.message ||
+                errors.day?.message ||
+                errors.year?.message}
             </ErrorMessage>
           )}
         </div>
         <div className="grid grid-cols-[3fr_3fr_4.5fr] gap-[14px] tablet:grid-cols-[2fr_2fr_3fr]">
           <input
-            value={month}
-            className={`${dateError ? "border-red text-red placeholder:text-red/50" : ""}`}
-            onChange={(e) => handleInputChange(e, setMonth, setDateError)}
             id="month"
             placeholder="MM"
             type="number"
+            {...register("month", {
+              required: "This field is incomplete",
+              min: { value: 1, message: "invalid date" },
+              max: { value: 12, message: "invalid date" },
+              onChange: () => clearListOfErrors(["month", "day", "year"]),
+            })}
+            className={`${dateError ? "border-red text-red placeholder:text-red/50" : ""}`}
           />
           <input
-            value={day}
-            className={`${dateError ? "border-red text-red placeholder:text-red/50" : ""}`}
-            onChange={(e) => handleInputChange(e, setDay, setDateError)}
             placeholder="DD"
             type="number"
+            {...register("day", {
+              required: "This field is incomplete",
+              min: { value: 1, message: "invalid date" },
+              max: { value: 31, message: "invalid date" },
+              onChange: () => clearListOfErrors(["month", "day", "year"]),
+            })}
+            className={`${dateError ? "border-red text-red placeholder:text-red/50" : ""}`}
           />
           <input
-            value={year}
-            className={`${dateError ? "border-red text-red placeholder:text-red/50" : ""}`}
-            onChange={(e) => handleInputChange(e, setYear, setDateError)}
             placeholder="YYYY"
             type="number"
+            {...register("year", {
+              required: "This field is incomplete",
+              min: {
+                value: currentDate.getFullYear(),
+                message: "invalid date",
+              },
+              max: {
+                value: currentDate.getFullYear() + 6,
+                message: "invalid date",
+              },
+              onChange: () => clearListOfErrors(["month", "day", "year"]),
+            })}
+            className={`${dateError ? "border-red text-red placeholder:text-red/50" : ""}`}
           />
         </div>
       </div>
@@ -137,25 +194,44 @@ export function ReservationForm() {
           </label>
           {timeError && (
             <ErrorMessage className="max-tablet:absolute max-tablet:left-[100px] max-tablet:top-0">
-              {timeError}
+              {errors.hour?.message || errors.minutes?.message}
             </ErrorMessage>
           )}
         </div>
         <div className="grid grid-cols-[3fr_3fr_4.5fr] gap-[14px] tablet:grid-cols-[2fr_2fr_3fr]">
           <input
-            value={hour}
-            className={`${timeError ? "border-red text-red placeholder:text-red/50" : ""}`}
-            onChange={(e) => handleInputChange(e, setHour, setTimeError)}
             id="hour"
             placeholder="09"
             type="number"
+            {...register("hour", {
+              required: "This field is incomplete",
+              min: {
+                value: 1,
+                message: "Invalid date",
+              },
+              max: {
+                value: 12,
+                message: "Invalid date",
+              },
+              validate: (hour) => {
+                return Number.parseInt(hour) === 12 && period === "AM"
+                  ? "Invalid time"
+                  : true;
+              },
+              onChange: () => clearListOfErrors(["hour", "minutes"]),
+            })}
+            className={`${timeError ? "border-red text-red placeholder:text-red/50" : ""}`}
           />
           <input
-            value={minutes}
-            className={`${timeError ? "border-red text-red placeholder:text-red/50" : ""}`}
-            onChange={(e) => handleInputChange(e, setMinutes, setTimeError)}
             placeholder="00"
             type="number"
+            {...register("minutes", {
+              required: "This field is incomplete",
+              min: { value: 0, message: "invalid time" },
+              max: { value: 59, message: "invalid time" },
+              onChange: () => clearListOfErrors(["hour", "minutes"]),
+            })}
+            className={`${timeError ? "border-red text-red placeholder:text-red/50" : ""}`}
           />
           <div className="relative">
             <button
@@ -175,14 +251,26 @@ export function ReservationForm() {
                 <li
                   className={`${period === "AM" ? "bg-[url('/images/icons/icon-check.svg')] bg-[length:13px_11px] bg-[top_8px_left_17px] bg-no-repeat" : ""}`}
                 >
-                  <button type="button" onClick={() => setPeriod("AM")}>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setPeriod("AM");
+                      clearListOfErrors(["hour", "minutes"]);
+                    }}
+                  >
                     AM
                   </button>
                 </li>
                 <li
                   className={`${period === "PM" ? "bg-[url('/images/icons/icon-check.svg')] bg-[length:13px_11px] bg-[top_8px_left_17px] bg-no-repeat" : ""}`}
                 >
-                  <button type="button" onClick={() => setPeriod("PM")}>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setPeriod("PM");
+                      clearListOfErrors(["hour", "minutes"]);
+                    }}
+                  >
                     PM
                   </button>
                 </li>
@@ -206,8 +294,8 @@ export function ReservationForm() {
           onClick={() => setNumberOfPeople((n) => n + 1)}
         ></button>
       </div>
-      <Button theme={"light"} type={"submit"}>
-        Make Reservation
+      <Button theme={"light"} type={"submit"} disabled={isSubmitting}>
+        {isSubmitting ? "Loading" : "Make Reservation"}
       </Button>
     </form>
   );
