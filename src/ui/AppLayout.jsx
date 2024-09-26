e509426e92bc30ca8f1c5da00f7ad5cb8f93ee23
lleonardus <leonardus.wizard@gmail.com>
2024-09-26 14:32:14 -0300

    removing grid class

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -4,7 +4,7 @@ import { Footer } from "./Footer";
 export function AppLayout() {
   return (
     <div className="flex h-screen w-screen justify-center font-spartan">
-      <div className="grid h-full w-full max-w-[1920px]">
+      <div className="h-full w-full max-w-[1920px]">
         <Outlet />
         <Footer />
       </div>
