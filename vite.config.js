548f2a7091829a6f07255610a56c06e7f175a457
lleonardus <leonardus.wizard@gmail.com>
2024-09-29 19:04:31 -0300

    returning to the previous config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
-  plugins: [react(), process.env.NODE_ENV !== "production" && eslint()].filter(
-    Boolean,
-  ), // Filtra plugins inválidos
+  plugins: [react(), eslint()],
 });
