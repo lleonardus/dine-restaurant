a50f3a6482822a6665b6c5e45e4a7c5313d2deaa
lleonardus <leonardus.wizard@gmail.com>
2024-09-29 16:10:08 -0300

    disabling eslint in production

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint({ eslintPath: "./.eslintrc.cjs" })],
+  plugins: [react(), process.env.NODE_ENV !== "production" && eslint()].filter(
+    Boolean,
+  ), // Filtra plugins inválidos
 });
