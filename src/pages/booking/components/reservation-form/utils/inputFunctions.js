f5c1bb36a7a3491c88df47ce2d0ba3a4b4ecfed4
lleonardus <leonardus.wizard@gmail.com>
2024-10-03 15:08:28 -0300

    creating the focusOnPreviousSiblingWhenEmpty function. It aims to focus onthe previous input when the user presses Backspace and the current input is empty

--- src/pages/booking/components/reservation-form/utils/inputFunctions.js
+++ src/pages/booking/components/reservation-form/utils/inputFunctions.js
@@ -1,12 +1,25 @@
-export { focusOnSiblingWhenComplete };
+export { focusOnSiblingWhenComplete, focusOnPreviousSiblingWhenEmpty };
 
-function focusOnSiblingWhenComplete(e) {
+function focusOnSiblingWhenComplete(e, maxLength) {
   const currentValue = e.target.value;
-  const maxLength = e.target.max.length || 2;
 
   if (currentValue.length === maxLength) {
     const form = Array.from(e.target.form);
     const siblingNodeIndex = form.indexOf(e.target) + 1;
+    const siblingNode = form[siblingNodeIndex];
+
+    siblingNode?.focus();
+  }
+}
+
+//focus on previous sibling when the user presses backspace and the current input is empty
+function focusOnPreviousSiblingWhenEmpty(e) {
+  const currentValue = e.target.value;
+  if (e.key === "Backspace" && !currentValue.length) {
+    e.preventDefault();
+
+    const form = Array.from(e.target.form);
+    const siblingNodeIndex = form.indexOf(e.target) - 1;
     const siblingNode = form[siblingNodeIndex];
 
     siblingNode?.focus();
